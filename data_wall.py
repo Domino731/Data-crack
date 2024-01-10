b4ae4f2812ff08fe3aa254889d9dc196c386b14c
Domino731 <d.orzechowski18@gmail.com>
2024-01-10 19:00:25 +0100

    displaying walls & detecting data crack

--- data_wall.py
+++ data_wall.py
@@ -1,15 +1,46 @@
+import random
+
 import pygame
 
-data_wall_width = 30
-data_wall_height = 30
-space_between_columns = 40
+data_wall_width = 50
+data_wall_height = 140
+space_between_columns = 35
+window_width = 800
+max_bottom = 500
+max_top = 40
+
+
+def create_random_wall_x():
+    return random.randint(max_top, max_bottom)
 
 
 class DataWall():
     def __init__(self, x, y):
+        self.is_cracked = False
+        self.velocity = 3
         self.x = x
-        self.y = y
+        self.y1 = y
+        self.y2 = y + data_wall_height + space_between_columns
+        self.top_y2 = self.y1 + data_wall_height
+        self.bot_y1 = self.y1 + data_wall_height + space_between_columns
 
     def draw(self, screen):
-        pygame.draw.rect(screen, "white", pygame.Rect(self.x, self.y, data_wall_width, data_wall_height))
-        pygame.draw.rect(screen, "white", pygame.Rect(self.x, self.y + space_between_columns, data_wall_width, data_wall_height))
+        pygame.draw.rect(screen, "white", pygame.Rect(self.x, self.y1, data_wall_width, data_wall_height))
+        pygame.draw.rect(screen, "white",
+                         pygame.Rect(self.x, self.y2 + space_between_columns, data_wall_width, data_wall_height))
+
+    def update(self):
+        if self.is_cracked: return
+
+        # Check if the top edge has reached the screen boundary
+        if self.y1 <= max_top:
+            self.velocity = abs(self.velocity)  # Reverse the velocity to bounce down
+
+        # Check if the bottom edge has reached the screen boundary
+        if self.y2 >= max_bottom:
+            self.velocity = -abs(self.velocity)  # Reverse the velocity to bounce up
+
+        self.y1 += self.velocity
+        self.y2 += self.velocity
+        self.top_y2 += self.velocity
+        self.bot_y1 += self.velocity
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import math
 
 import pygame
 
-from data_wall import DataWall
+from data_wall import DataWall, create_random_wall_x
 
 # pygame setup
 pygame.init()
@@ -19,30 +19,75 @@ container_padding = 30
 container_offset = math.floor(container_padding / 2)
 container_width = width - container_padding
 container_height = height - container_padding
+
+
 def draw_container():
-    pygame.draw.rect(screen, "red", pygame.Rect(container_offset, container_offset,container_width, container_height), 2)
+    pygame.draw.rect(screen, "red", pygame.Rect(container_offset, container_offset, container_width, container_height),
+                     2)
+
+
+bridge_offset_y = (height / 2) - 15
+bridge_height = 30
 
 
 def draw_bridge():
-    pygame.draw.rect(screen, "red", pygame.Rect(container_offset, (height / 2) - 15, container_width, 30))
+    pygame.draw.rect(screen, "red", pygame.Rect(container_offset, bridge_offset_y, container_width, bridge_height))
 
 
 data_walls = []
 
-data_walls.append(DataWall(40, 40))
+walls_padding = 40
+space_for_walls = width - 2 * 40
+walls_amount = 2
+space_between_walls = 40
+start_x = 40
+for i in range(walls_amount):
+    data_walls.append(DataWall(start_x, create_random_wall_x()))
+    start_x += 40 + space_between_walls
+
 
 def draw_data_walls():
     for wall in data_walls:
         wall.draw(screen)
 
-print(width, height)
+
+def update_data_walls():
+    for wall in data_walls:
+        wall.update()
+
+
+current_wall_index = 0
+current_wall = data_walls[current_wall_index]
+
+
+def check_if_data_crack():
+    y1 = current_wall.top_y2
+    y2 = current_wall.bot_y1
+    bridge_y1 = bridge_offset_y - 10
+    bridge_y2 = bridge_offset_y + bridge_height + 10
+    bridge_range = range(int(bridge_y1), int(bridge_y2))
+    print(y1, y2)
+    print("range: ", bridge_range)
+    if y1 < bridge_y1 and bridge_y2 > y2:
+        current_wall.is_cracked = True
+        current_wall_index = 1
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                check_if_data_crack()
+    keys = pygame.key.get_pressed()
 
+    # Check if the spacebar is pressed
+    # if keys[pygame.K_SPACE]:
+    #     check_if_data_crack()
+
+    update_data_walls()
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
 
