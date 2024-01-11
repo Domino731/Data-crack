43bb52a18bdc8d1115a33f2feb4fe05748bed0bd
Domino731 <d.orzechowski18@gmail.com>
2024-01-11 20:28:31 +0100

    optimization

--- main.py
+++ main.py
@@ -1,116 +1,115 @@
-# Example file showing a circle moving on screen
 import math
-
 import pygame
-
 from data_wall import DataWall, create_random_wall_x
 
-# pygame setup
+# Constants
+SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
+CONTAINER_PADDING = 30
+BRIDGE_HEIGHT = 30
+WALLS_AMOUNT = 2
+SPACE_BETWEEN_WALLS = 40
+START_X = 40
+
+# Initialize pygame
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 running = True
-dt = 0
 
+# Player setup
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-width, height = pygame.display.get_surface().get_size()
 
-container_padding = 30
-container_offset = math.floor(container_padding / 2)
-container_width = width - container_padding
-container_height = height - container_padding
+# Container setup
+container_offset = math.floor(CONTAINER_PADDING / 2)
+container_width = SCREEN_WIDTH - CONTAINER_PADDING
+container_height = SCREEN_HEIGHT - CONTAINER_PADDING
+
+# Bridge setup
+bridge_offset_y = (SCREEN_HEIGHT / 2) - 15
 
 
 def draw_container():
-    pygame.draw.rect(screen, "red", pygame.Rect(container_offset, container_offset, container_width, container_height),
-                     2)
-
-
-bridge_offset_y = (height / 2) - 15
-bridge_height = 30
+    pygame.draw.rect(
+        screen, "red",
+        pygame.Rect(container_offset, container_offset, container_width, container_height),
+        2
+    )
 
 
 def draw_bridge():
-    pygame.draw.rect(screen, "red", pygame.Rect(container_offset, bridge_offset_y, container_width, bridge_height))
+    pygame.draw.rect(
+        screen, "red",
+        pygame.Rect(container_offset, bridge_offset_y, container_width, BRIDGE_HEIGHT)
+    )
 
 
-data_walls = []
-
-walls_padding = 40
-space_for_walls = width - 2 * 40
-walls_amount = 2
-space_between_walls = 40
-start_x = 40
-for i in range(walls_amount):
-    data_walls.append(DataWall(start_x, create_random_wall_x()))
-    start_x += 40 + space_between_walls
+def create_data_walls():
+    data_walls = []
+    x_position = START_X
+    for _ in range(WALLS_AMOUNT):
+        data_walls.append(DataWall(x_position, create_random_wall_x()))
+        x_position += SPACE_BETWEEN_WALLS + START_X
+    return data_walls
 
 
-def draw_data_walls():
-    for wall in data_walls:
-        wall.draw(screen)
+class Game:
+    def __init__(self):
+        self.current_wall_index = 0
+        self.data_walls = create_data_walls()
+        self.current_wall = self.data_walls[self.current_wall_index]
+
+    def draw_walls(self):
+        for wall in self.data_walls:
+            wall.draw(screen)
+
+    def update(self):
+        for wall in self.data_walls:
+            wall.update()
+
+    def reset_walls(self):
+        self.current_wall_index = 0
+        self.current_wall = self.data_walls[self.current_wall_index]
+        for wall in self.data_walls:
+            wall.is_cracked = False
+
+    def check_if_data_crack(self):
+        y1, y2 = self.current_wall.top_y2, self.current_wall.bot_y1
+        bridge_y1, bridge_y2 = bridge_offset_y - 10, bridge_offset_y + BRIDGE_HEIGHT + 10
+        bridge_range = range(int(bridge_y1), int(bridge_y2))
+
+        print(y1, y2)
+        print("range: ", bridge_range)
+
+        if y1 < bridge_y1 and bridge_y2 > y2:
+            self.current_wall.is_cracked = True
+            self.current_wall_index += 1
+            if self.current_wall_index < len(self.data_walls):
+                self.current_wall = self.data_walls[self.current_wall_index]
+            else:
+                print("GAME OVER")
+        else:
+            self.reset_walls()
 
 
-def update_data_walls():
-    for wall in data_walls:
-        wall.update()
-
-
-current_wall_index = 0
-current_wall = data_walls[current_wall_index]
-
-
-def check_if_data_crack():
-    y1 = current_wall.top_y2
-    y2 = current_wall.bot_y1
-    bridge_y1 = bridge_offset_y - 10
-    bridge_y2 = bridge_offset_y + bridge_height + 10
-    bridge_range = range(int(bridge_y1), int(bridge_y2))
-    print(y1, y2)
-    print("range: ", bridge_range)
-    if y1 < bridge_y1 and bridge_y2 > y2:
-        current_wall.is_cracked = True
-        current_wall_index = 1
+game = Game()
 
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_SPACE:
-                check_if_data_crack()
-    keys = pygame.key.get_pressed()
+                game.check_if_data_crack()
 
-    # Check if the spacebar is pressed
-    # if keys[pygame.K_SPACE]:
-    #     check_if_data_crack()
+    game.update()
 
-    update_data_walls()
-    # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
-
     pygame.draw.circle(screen, "yellow", player_pos, 40)
     draw_container()
     draw_bridge()
-    draw_data_walls()
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+    game.draw_walls()
 
-    # flip() the display to put your work on screen
     pygame.display.flip()
-
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
     dt = clock.tick(60) / 1000
 
 pygame.quit()
